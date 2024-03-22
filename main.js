a63404c25cb1b37b6387558547c2b414b18dc78b
andreyhong <andreyhong24@gmail.com>
2024-03-22 14:18:22 -0700

    add contact

---
+++ main.js
@@ -0,0 +1,35 @@
+function toggleDetail(e) {
+    const target = $(e.target)
+
+if($(target).hasClass("active")) {
+    $(target).html("More Info").removeClass("active")
+} else {
+
+    $(target).html("Less Info").addclass("active")
+}
+
+    const item = $(target).parents(".about-exp-item")
+    const detail = $(item).children(".about-exp-item-detail")
+    $(detail).slideToggle()
+}
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const email = $("#inp_subject")
+    const email = $("#inp_message")
+
+    if(!$(email).val()) {
+        alert("Email is required")
+    } else if (!$(subject).val()) {
+        alert("Subject is required")
+    } else if (!$(message).val()) {
+        alert("Message is required")
+    } else if {
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+        alert("Form Submitted")
+    }
+
+}
