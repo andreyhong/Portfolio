6a0b06d236add06a529ccf7d610b843c9399c18e
andreyhong <andreyhong24@gmail.com>
2024-03-22 14:52:56 -0700

    tidy it up

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
 function toggleDetail(e) {
-    const target = $(e.target)
+    const target = $(e.target);
 
-if($(target).hasClass("active")) {
-    $(target).html("More Info").removeClass("active")
-} else {
+    if ($(target).hasClass("active")) {
+        $(target).html("More Info").removeClass("active");
+    } else {
+        $(target).html("Less Info").addClass("active");
+    }
 
-    $(target).html("Less Info").addclass("active")
-}
-
-    const item = $(target).parents(".about-exp-item")
-    const detail = $(item).children(".about-exp-item-detail")
-    $(detail).slideToggle()
+    const item = $(target).parents(".about-exp-item");
+    const detail = $(item).children(".about-exp-item-detail");
+    $(detail).slideToggle();
 }
 
 function onFormSubmit(e) {
-    e.preventDefault()
-    const email = $("#inp_email")
-    const email = $("#inp_subject")
-    const email = $("#inp_message")
+    e.preventDefault();
+    const email = $("#inp_email").val();
+    const subject = $("#inp_subject").val();
+    const message = $("#inp_message").val();
 
-    if(!$(email).val()) {
-        alert("Email is required")
-    } else if (!$(subject).val()) {
-        alert("Subject is required")
-    } else if (!$(message).val()) {
-        alert("Message is required")
-    } else if {
-        $(email).val("")
-        $(subject).val("")
-        $(message).val("")
-        alert("Form Submitted")
+    if (!email) {
+        alert("Email is required");
+    } else if (!subject) {
+        alert("Subject is required");
+    } else if (!message) {
+        alert("Message is required");
+    } else {
+        $("#inp_email").val("");
+        $("#inp_subject").val("");
+        $("#inp_message").val("");
+        alert("Form Submitted");
     }
-
 }
+
